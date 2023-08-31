ada2d5de8ed1debde69b7770b618dd74f26c13db
CatherinCruz <cccruzt@academia.usbbog.edu.co>
2023-08-31 15:11:41 -0500

    parcial

---
+++ Catheri_pasm/Corte1/parcial/Blackjack.py
@@ -0,0 +1,28 @@
+import random as r
+
+def dos(conteo):
+    for o in range (conteo):
+        print(conteo[s],'nuevacarta')
+def r(s):
+    input('desea pedir una carta: ')
+    n='no'
+    y='si'
+    if (y =='si'):
+        print(conteo)
+    pal=''
+    s='star'
+    while pal !='finaliza la apuesta':
+        J=10
+        Q=10
+        K=10
+        A=1+s
+        s=(A,2,3,4,5,6,7,8,9,J,Q,K)
+        while A>[J,Q,K]:
+            print (s)
+        pal=input(f'finaliza la apuesta?:',{y/n})
+
+    
+if __name__=='__main__':
+    conteo='Yes/No'
+    r (conteo)
+    dos(conteo)
---
+++ Catheri_pasm/Corte1/parcial/blackjack2.py
@@ -0,0 +1,18 @@
+def y ():
+    r= b+v
+    
+    b=(A,2,3,4,5,6,7,8,9,J,K,Q)
+    A=1
+    J=10
+    K=10
+    Q=10
+if (Nuevacarta==21):
+    print('Blackjack:')
+
+
+
+
+
+if __name__=='__main__':
+    Nuevacarta='y/n'
+    y ()
